8d07d84094b8b5c0999099dc32a3edcf8109c93c
Rachel <rachel@rachelrockaway.com>
2015-11-17 03:48:32 -0500

    Add login view controller.

---
+++ Inventory/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Inventory
+//
+//  Created by Rachel Stevenson on 11/16/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import Parse
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginTapped(sender: AnyObject) {
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        PFUser.logInWithUsernameInBackground(username!, password: password!) {
+            (user: PFUser?, error: NSError?)-> Void in
+        
+        if (error == nil) {
+            // Successfully logged in.
+            
+            print("Successfully Logged In.")
+            
+        } else {
+            // Error while logging in.
+            
+            var alertController = UIAlertController(title: "Error", message: "Error, Incorrect Username/Password.", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            
+            }
+        }
+
+    }
+}
